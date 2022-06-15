872e463a3674f8c2e5af45c06fb471e758bb4095
kauaumonougueira <kaua_kna@hotmail.com>
2022-06-15 11:49:32 -0300

    feat: AppBar refatored

--- login_react/src/components/AppBar.js
+++ login_react/src/components/AppBar.js
@@ -3,10 +3,11 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 
-export default function Appbar() {
+export default function ButtonAppBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -21,8 +22,9 @@ export default function Appbar() {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Spring Boot React Full Stack Application
+            News
           </Typography>
+          <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
     </Box>
